0cb26fd1eccc0d9388d29e2001a6a45613fcb74b
Asier Villar <asiervillar@outlook.es>
2023-08-09 23:49:47 +0200

    ADD: Client demo

---
+++ client/app.py
@@ -0,0 +1,79 @@
+from functools import lru_cache
+from typing import Any
+
+import cv2
+import numpy as np
+import pandas as pd
+import streamlit as st
+import torch
+from clearml import Model
+from streamlit_drawable_canvas import st_canvas
+
+
+def get_model() -> list[torch.nn.Module]:
+    my_models = Model.query_models(
+        project_name="Number_detection",
+        tags=["production"],
+    )
+
+    model_list = []
+    for model in my_models:
+        model_path = model.get_local_copy()
+        model_list.append(torch.jit.load(model_path))
+
+    print(len(model_list))
+
+    return model_list
+
+
+def make_prediction(image: np.ndarray[Any, Any]) -> list[int]:
+    model_list = get_model()
+    tensor = torch.tensor(np.array([image])).float()
+
+    result_list = []
+    for model in model_list:
+        result = model(tensor).tolist()
+        value = np.argmax(result, axis=1)[0]
+        result_list.append(value)
+
+    return result_list
+
+
+def main() -> None:
+    drawing_mode = "freedraw"
+    stroke_width = 15
+    stroke_color = "#000000"
+    bg_color = "#FFFFFF"
+    realtime_update = True
+
+    # Create a canvas component
+    canvas_result = st_canvas(
+        stroke_width=stroke_width,
+        stroke_color=stroke_color,
+        background_color=bg_color,
+        update_streamlit=realtime_update,
+        height=280,
+        width=280,
+        drawing_mode=drawing_mode,
+        key="full_app",
+    )
+
+    if canvas_result.image_data is not None:
+        original_image = canvas_result.image_data
+        gray_image = cv2.cvtColor(original_image, cv2.COLOR_RGBA2GRAY)
+        resized_image = 255 - cv2.resize(gray_image, (28, 28))
+
+        image_array = np.array(resized_image.tolist())
+        image_array = np.reshape(image_array, (1, 28, 28))
+        st.image(resized_image)
+
+        if st.button("Predict"):
+            result = make_prediction(image_array)
+            # Show the result
+            st.text(result)
+
+
+if __name__ == "__main__":
+    st.set_page_config(page_title="Streamlit Drawable Canvas Demo", page_icon=":rocket:")
+    st.title("Drawable Canvas Demo")
+    main()
--- src/settings/params.py
+++ src/settings/params.py
@@ -13,7 +13,7 @@ class TrainingParams(TypedDict):
 TRAINING_PARAMS: TrainingParams = {
     "lr": 1e-3,
     "batch_size": 32,
-    "n_epochs": 2,
+    "n_epochs": 5,
     "seed": 42,
     "device": "cuda",
     "is_checkpoint": False,

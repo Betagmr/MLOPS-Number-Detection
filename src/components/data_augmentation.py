b8df48db0cd6329b311305f662f9311cbc346441
Asier Villar <asiervillar@outlook.es>
2023-08-11 15:17:54 +0200

    ADD: Data Augmentation

---
+++ src/components/data_augmentation.py
@@ -0,0 +1,23 @@
+from typing import Any
+
+import numpy as np
+import torch
+from torchvision import transforms as T
+
+
+def data_augmentation(
+    x_data: np.ndarray[Any, Any],
+    y_data: np.ndarray[Any, Any],
+    translate: tuple[float, float] = (0.2, 0.25),
+    scale: tuple[float, float] = (0.55, 0.77),
+) -> tuple[np.ndarray[Any, Any], np.ndarray[Any, Any]]:
+    augmentation = T.Compose(
+        [
+            T.RandomRotation(10),
+            T.RandomAffine(0, translate=translate, scale=scale),
+        ]
+    )
+
+    x_data = augmentation(torch.from_numpy(x_data))
+
+    return np.array(x_data), y_data

986e03cdba58b5bd00c4227167dc72af4efc3253
Asier Villar <asiervillar@outlook.es>
2023-08-16 01:43:34 +0200

    REM: Task dependency on save model

--- src/components/save_model.py
+++ src/components/save_model.py
@@ -2,15 +2,10 @@ from pathlib import Path
 
 import pytorch_lightning as plt
 import torch
-from clearml import OutputModel, Task
 
 
-def save_model(model: plt.LightningModule, task: Task = None) -> None:
+def save_model(model: plt.LightningModule) -> None:
     model_path = Path() / "model.pt"
     script = model.to_torchscript()
 
     torch.jit.save(script, model_path)
-
-    if task:
-        output_model = OutputModel(task=task, tags=["lightning"])
-        output_model.update_weights(model_path)
